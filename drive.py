4ce7bf06c1b0a2394ac7296c25c6d627e1e019b1
bob ding <4655609+bobd988@users.noreply.github.com>
2019-03-10 17:58:56 -0700

    fix: add output image folder

--- drive.py
+++ drive.py
@@ -11,6 +11,9 @@ from PIL import Image
 from flask import Flask
 from keras.models import model_from_json
 import scipy.misc
+import os
+import shutil
+from datetime import datetime
 
 #tf.python.control_flow_ops = tf
 
@@ -18,6 +21,9 @@ sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
+MAX_SPEED = 25
+MIN_SPEED = 10
+speed_limit = MAX_SPEED
 
 
 def crop(image, top_percent, bottom_percent):
@@ -35,34 +41,51 @@ def resize(image, new_dim):
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
 
-    # The current throttle of the car
-    throttle = data["throttle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
 
-    # The current speed of the car
-    speed = data["speed"]
+        # The current speed of the car
+        speed = data["speed"]
 
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
 
-    #image_array = crop(image_array, 0.35, 0.1)
-    #image_array = resize(image_array, new_dim=(66, 200))
-    image_array = helper.preprocess(image_array)
-    transformed_image_array = image_array[None, :, :, :]
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
 
-    # This model currently assumes that the features of the model are just the images. Feel free to change this.
+        try:
+            image_array = np.asarray(image)
+            #image_array = crop(image_array, 0.35, 0.1)
+            #image_array = resize(image_array, new_dim=(66, 200))
+            image_array = helper.preprocess(image_array)
+            transformed_image_array = image_array[None, :, :, :]
 
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.3
+            # This model currently assumes that the features of the model are just the images. Feel free to change this.
 
-    print('{:.5f}, {:.1f}'.format(steering_angle, throttle))
+            steering_angle = float(model.predict(transformed_image_array, batch_size=1))
 
-    send_control(steering_angle, throttle)
+            global speed_limit
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED  # slow down
+            else:
+                speed_limit = MAX_SPEED
+            throttle = 1.0 - steering_angle ** 2 - (speed / speed_limit) ** 2
+
+            print('{:.5f}, {:.1f}'.format(steering_angle, throttle))
+
+            send_control(steering_angle, throttle)
+        except Exception as e:
+            print(e)
+    else:
+
+        sio.emit('manual', data={}, skip_sid=True)
 
 
 @sio.on('connect')
@@ -82,13 +105,33 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument('model', type=str,
                         help='Path to model definition json. Model weights should be on the same path.')
+
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
     args = parser.parse_args()
+
     with open(args.model, 'r') as jfile:
         model = model_from_json(json.load(jfile))
 
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
     model.load_weights(weights_file)
+
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
--- model.py
+++ model.py
@@ -6,17 +6,13 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 import helper
 
-STEERING_COEFFICIENT = 0.22
-#col, row = 200,66
+# parameters to adjust
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
-#bob tf.python.control_flow_ops = tf
-number_of_epochs = 8
-number_of_samples_per_epoch = 20244
-number_of_validation_samples = 6400
-learning_rate = 1e-4
-activation_relu = 'relu'
-
+NUMBER_OF_EPOCHS = 8
+NUMBER_OF_SAMPLES_PER_EPOCH = 20224
+NUMBER_OF_VALIDATION_SAMPLES = 6400
+LEARNING_RATE = 1e-4
 
 
 # the input is directly 66,200,3 . Not using resize to 64X64
@@ -29,23 +25,23 @@ def build_model():
 
    # starts with five convolutional and maxpooling layers
    model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
    model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
    model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
    #model.add(Dropout(0.5))
@@ -53,16 +49,16 @@ def build_model():
 
    # Next, five fully connected layers
    model.add(Dense(1164))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
 
    model.add(Dense(100))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
 
    model.add(Dense(50))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
 
    model.add(Dense(10))
-   model.add(Activation(activation_relu))
+   model.add(Activation('relu'))
 
    model.add(Dense(1))
 
@@ -73,26 +69,24 @@ def build_model():
 
 
 def train_model(model):
-    """
-    Train the model
-    """
+
     checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
                                  save_best_only=True,
                                  mode='auto')
 
-    model.compile(optimizer=Adam(learning_rate), loss="mse", )
+    model.compile(optimizer=Adam(LEARNING_RATE), loss="mse", )
 
     # create generators for training and validation
     train_generator = helper.generator_training()
     validation_generator = helper.generator_validation()
 
     history = model.fit_generator(train_generator,
-                                  samples_per_epoch=number_of_samples_per_epoch,
-                                  nb_epoch=number_of_epochs,
+                                  samples_per_epoch=NUMBER_OF_SAMPLES_PER_EPOCH,
+                                  nb_epoch=NUMBER_OF_EPOCHS,
                                   validation_data=validation_generator,
-                                  nb_val_samples=number_of_validation_samples,
+                                  nb_val_samples=NUMBER_OF_VALIDATION_SAMPLES,
                                   callbacks=[checkpoint],
                                   verbose=1)
 
